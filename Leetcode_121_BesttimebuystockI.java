1f055515a51792f4c5639a4477b4e11c7e738845
sandylujob <sandylu80@gmail.com>
2015-08-03 02:31:03 -0700

    Add best time to stock I and II

--- Leetcode_121_BesttimebuystockI.java
+++ Leetcode_121_BesttimebuystockI.java
@@ -3,8 +3,19 @@
 *If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
 */
 
+import java.lang.Math;
+
 public class Solution {
     public int maxProfit(int[] prices) {
-        
+        int maxVal = 0;
+        int minVal = Integer.Max_VALUE;
+
+        for (int i = 0; i< prices.length; i++) {
+        	if (prices[i] < minVal) {
+        		minVal = prices[i] ;
+        	}
+        	maxVal = Math.max(prices[i]-minVal, maxVal); 
+        }
+        return maxVal ;
     }
 }
